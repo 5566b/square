3645f4ca3f936fa3f3718d390c87d95105c09943
5566b <106473242+5566b@users.noreply.github.com>
2023-07-07 22:29:10 +0300

    Add files via upload

---
+++ square/ConsoleApp4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("enter 1 side ");
+
+var input1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter 2 side ");
+var input2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("S rectangle ");
+var input3 = (input1 * input2);
+
+Console.WriteLine(input3);
